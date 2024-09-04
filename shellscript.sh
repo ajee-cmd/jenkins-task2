5e41abf837cae646ac4bd6a6f15bdb2eb98acede
ajee-cmd <priyaethiraj1298@gmail.com>
2024-09-04 13:20:01 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,2 +1,25 @@
-#!/bin/bash
-echo "Hello, World!" > output.txt
+pipeline {
+    agent any
+    
+    environment {
+        // GIT_URL = 'https://github.com/christine-robinson/zen-devops-webhook-test.git'
+        GIT_URL = '<your clone url>'
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(
+                    branches: [[name: '*/main']], 
+                    extensions: [], 
+                    userRemoteConfigs: [[url: "${env.GIT_URL}"]]
+                )
+            }
+        }
+        stage('List Files'){
+            steps {
+                bat 'dir'
+            }
+        }
+    }
+}
